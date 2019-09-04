7693f18a557c447c3a7b3878e959da6b2eec5944
Gabriel Lima <gabriellfsouza@gmail.com>
2019-09-04 06:03:21 -0300

    chore: first implementation of a TDD approach (phase 3, refactor)

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -13,4 +13,16 @@ describe('User', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('should not be able to register with duplicated email', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Gabriel Lima',
+        email: 'gabriellfsouza@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
+  });
 });
